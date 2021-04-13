94fd3e7f19d587d6a980fccf5228627ceea22a21
EomJunyoung <theoneday737@naver.com>
2021-04-13 17:37:22 +0900

    add bottom navigation view and etc(add menu item, drawable image)

--- app/src/main/java/com/example/ucproject/MainActivity.kt
+++ app/src/main/java/com/example/ucproject/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.ucproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ucproject.fragment.FragmentOne
+import com.example.ucproject.fragment.FragmentThree
+import com.example.ucproject.fragment.FragmentTwo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentOne by lazy { FragmentOne() }
+    private val fragmentTwo by lazy { FragmentTwo() }
+    private val fragmentThree by lazy { FragmentThree() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigationBar()
     }
+
+    private fun initNavigationBar() {
+        bnv_main.run {
+            setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.first -> {
+                        changeFragment(fragmentOne)
+                    }
+                    R.id.second -> {
+                        changeFragment(fragmentTwo)
+                    }
+                    R.id.third -> {
+                        changeFragment(fragmentThree)
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.first
+        }
+
+
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_container, fragment)
+            .commit()
+    }
+
 }
