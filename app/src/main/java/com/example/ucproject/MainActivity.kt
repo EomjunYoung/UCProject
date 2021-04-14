457873dd22c2e4cf146ad5549fc61e4a95b672b7
EomJunyoung <theoneday737@naver.com>
2021-04-14 10:07:53 +0900

    add viewpager2 and interract viewpager-bottom navi

--- app/src/main/java/com/example/ucproject/MainActivity.kt
+++ app/src/main/java/com/example/ucproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ucproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.ucproject.fragment.FragmentOne
 import com.example.ucproject.fragment.FragmentThree
 import com.example.ucproject.fragment.FragmentTwo
@@ -14,13 +15,21 @@ class MainActivity : AppCompatActivity() {
     private val fragmentTwo by lazy { FragmentTwo() }
     private val fragmentThree by lazy { FragmentThree() }
 
+    private val fragments : List<Fragment> = listOf(fragmentOne, fragmentTwo, fragmentThree)
+
+    private val pagerAdapter : MainViewPagerAdapter by lazy {
+        MainViewPagerAdapter(this, fragments)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initNavigationBar()
+        initViewPager()
+        initNavigationBar2()
     }
 
+    //Bottom NavigationView의 아이콘 클릭시 Fragment 변화함수
     private fun initNavigationBar() {
         bnv_main.run {
             setOnNavigationItemSelectedListener {
@@ -39,7 +48,30 @@ class MainActivity : AppCompatActivity() {
             }
             selectedItemId = R.id.first
         }
+    }
 
+    //ViewPager2가 적용된 Bottom NavigationView (좌우 스와이핑시 Fragment 변화함수)
+    private fun initNavigationBar2() {
+
+        bnv_main.run {
+            setOnNavigationItemSelectedListener {
+                val page = when(it.itemId){
+                    R.id.first -> 0
+                    R.id.second -> 1
+                    R.id.third -> 2
+                    else -> 0
+                }
+
+                //viewpager의 현재화면은 currentItem(Int형)으로 받고
+                //BottomNavigationView의 현재아이콘은 currentItemId(리소스id(int))형으로 받음
+                if(page!= vp_main.currentItem){
+                    vp_main.currentItem = page
+                }
+
+                true
+            }
+            selectedItemId = R.id.first
+        }
 
     }
 
@@ -49,5 +81,29 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fl_container, fragment)
             .commit()
     }
+
+    private fun initViewPager(){
+        vp_main.run{
+            adapter = pagerAdapter
+            registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    val navigation = when(position){
+                        0 -> R.id.first
+                        1 -> R.id.second
+                        2 -> R.id.third
+                        else -> R.id.first
+                    }
+
+                    //viewpager의 현재화면은 currentItem(Int형)으로 받고
+                    //BottomNavigationView의 현재아이콘은 currentItemId(리소스id(int))형으로 받음
+                    if(bnv_main.selectedItemId != navigation){
+                        bnv_main.selectedItemId = navigation
+                    }
+                }
+            })
+        }
+    }
+
+
 
 }
